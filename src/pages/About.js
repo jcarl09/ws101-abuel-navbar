193ce4dd9d487f6b511972474f1dfe370ad35da2
jcarlo.git config --global user.name <jcarlo.abuel@gmail.com>
2024-11-26 20:10:21 +0800

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,9 +5,7 @@ export default function About() {
 
     <div className='text-div'>
         <h1>About</h1>
-        <p>"About"  in a navbar directs users to a page that provides information about the website or organization. <br/>
-             It typically includes the mission, background, and purpose of the site.<br/>
-              This helps users understand the site's goals and builds trust by offering transparency.</p>
+      
         
 
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,9 +2,11 @@
 
 export default function Contact() {
     return (
-        <p>"Contact Us" link in a navbar allows users to access information for reaching out to the website or organization. <br/>
-             This page typically includes contact details such as email, phone numbers, and a form for inquiries.<br/>
-             It helps users communicate directly, fostering engagement and support.</p>
+        <div className='text-div'>
+        <h1>Contact Us</h1>
+      
+        
 
+    </div>
 )
 }
